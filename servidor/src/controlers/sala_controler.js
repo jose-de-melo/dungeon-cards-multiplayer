1493ff520d1229559763ef4a2fef6b944404e7c1
Ricardovcn <ricardovitorcn@gmail.com>
2019-12-12 16:31:30 -0300

    corrigindo método

--- servidor/src/controlers/sala_controler.js
+++ servidor/src/controlers/sala_controler.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Sala = require('../models/sala');
 const Card = require('../models/card');
-const authMiddleware = require('../middlewares/auth')
 
 var lista = [];
 
@@ -32,8 +31,13 @@ for(i=0; i<6;i++){
     potion.bounty = 0;
     lista.push(potion);
 }
+
 //Monstros
+var herois  = ["androide","barbaro","templario","ninja","ceifadora","elfo","necromante"]
+
+//Heroi
 var monstros = ["alien.png","aranha.png","cogumelo.png","esqueleto.png","javali.png","medusa.png","morcego.png","zumbi.png"];
+
 for(i=0; i<8;i++){
     monster = new Card();
     monster.type = 2;
@@ -46,10 +50,7 @@ for(i=0; i<8;i++){
     lista.push(monster);
 }
 
-
 const router = express.Router();
-
-router.use(authMiddleware);
 
 
 router.get('/', async (req,res)=> {
@@ -84,7 +85,7 @@ router.delete('/:id', async (req,res)=> {
 });
 
 router.post('/join', async (req,res)=> {
-    const { nickname, socket, heroi, id_sala } = req.body;
+    const { nickname, socket, id_sala } = req.body;
 
     const sala = await Sala.findOne({ _id: id_sala})
     if(!sala) 
@@ -94,23 +95,13 @@ router.post('/join', async (req,res)=> {
     if(nplayers == 4) 
         return res.status(400).send({ error: "Esta sala está cheia.", id_sala}) 
 
-    sala.players.push({nickname, socket, heroi})    
-    arma = new Card();
-    arma.name = heroi;
-    arma.type = 1;
-    arma.image = heroi + ".png";
-    arma.level = 0;
-    arma.life = 0;
-    arma.damage = 0;
-    arma.bounty = 0;
-    lista.push(arma);
+    sala.players.push({nickname, socket})    
 
     const resp = await Sala.update({_id: sala.id }, sala)
     if(!resp.nModified) return res.status(400).send({ error: "Não foi possível entrar na sala.", id_sala})
 
     return res.send({ status: "O player entrou na sala.", num_players: (nplayers+1)})
 });
-
 
 //Esse é o metodo q vai iniciar a partida
 router.get('/iniciar/:id_partida', async (req, res) => {
@@ -128,9 +119,10 @@ router.get('/iniciar/:id_partida', async (req, res) => {
 
     
     sala.players.sort(randOrd);
+    herois.sort(randOrd);
     
     p = new Card();
-    p.name = sala.players[0].heroi;
+    p.name = herois[0];
     p.type = 0;
     p.nick = sala.players[0].nickname;
     p.level = 1;
@@ -140,9 +132,19 @@ router.get('/iniciar/:id_partida', async (req, res) => {
     p.x = 1;
     p.y = 1;
     sala.posicoes[1][1] = p;
+    arma = new Card();
+    arma.name = herois[0];
+    arma.type = 1;
+    arma.image = herois[0] + ".png";
+    arma.level = 0;
+    arma.life = 0;
+    arma.damage = 0;
+    arma.bounty = 0;
+    lista.push(arma);
+
 
     p = new Card();
-    p.name = sala.players[1].heroi;
+    p.name =  herois[1];
     p.type = 0;
     p.nick = sala.players[1].nickname;
     p.level = 1;
@@ -152,9 +154,18 @@ router.get('/iniciar/:id_partida', async (req, res) => {
     p.x = 1;
     p.y = 4;
     sala.posicoes[1][4] = p;
+    arma = new Card();
+    arma.name = herois[1];
+    arma.type = 1;
+    arma.image = herois[1] + ".png";
+    arma.level = 0;
+    arma.life = 0;
+    arma.damage = 0;
+    arma.bounty = 0;
+    lista.push(arma);
 
     p = new Card();
-    p.name = sala.players[2].heroi;
+    p.name =  herois[2];
     p.type = 0;
     p.nick = sala.players[2].nickname;
     p.level = 1;
@@ -164,9 +175,18 @@ router.get('/iniciar/:id_partida', async (req, res) => {
     p.x = 4;
     p.y = 1;
     sala.posicoes[4][1] = p;
+    arma = new Card();
+    arma.name = herois[2];
+    arma.type = 1;
+    arma.image = herois[2] + ".png";
+    arma.level = 0;
+    arma.life = 0;
+    arma.damage = 0;
+    arma.bounty = 0;
+    lista.push(arma);
 
     p = new Card();
-    p.name = sala.players[3].heroi;
+    p.name =  herois[3];
     p.type = 0;
     p.nick = sala.players[3].nickname;
     p.level = 1;
@@ -176,49 +196,58 @@ router.get('/iniciar/:id_partida', async (req, res) => {
     p.x = 4;
     p.y = 4;
     sala.posicoes[4][4] = p;
+    arma = new Card();
+    arma.name = herois[3];
+    arma.type = 1;
+    arma.image = herois[3] + ".png";
+    arma.level = 0;
+    arma.life = 0;
+    arma.damage = 0;
+    arma.bounty = 0;
+    lista.push(arma);
 
     // ATUALIZA MATRIZ PRO SOCKET
 
+
 });
-
-
 
 //Esse é o metodo q vai iniciar a partida
 router.post('/movimenta', async (req, res) => {
     const { x_atual, y_atual, id_sala, x_mov, y_mov } = req.body;
     
-    if(x>5)
+    if(x_mov>5)
         return res.send({ message: 0}) 
-    if(y>5)
+    if(y_mov>5)
         return res.send({ message: 0}) 
-    if(x<0)
+    if(x_mov<0)
         return res.send({ message: 0}) 
-    if(y<0)
+    if(y_mov<0)
         return res.send({ message: 0})
         
+    if(y_mod==sala.posicoes[x_atual][y_atual.x] && y_mod==sala.posicoes[x_atual][y_atual].y)
     sala.posicoes[x_atual][y_atual].x = x_mov;
-    sala.posicoes[x_atual][y_atual].x = y_mov;
+    sala.posicoes[x_atual][y_atual].y = y_mov;
 
     sala.posicoes[x_mov][y_mov] =  sala.posicoes[x_atual][y_atual];
 
     lista.sort(randOrd);
     sala.posicoes[x_atual][y_atual] = lista[0];
+
+    return res.send({ message: 1})
 });
-
-
-
 
 function randOrd() {
     return (Math.round(Math.random())-0.5);
 }
 
-//Função desnecessária caso optar por um jogo sem turnos.
+//Função que pode ser usada caso opte por ter turnos.
 router.get('/rolar_dado', async (req, res) => {
     const dado =  [1, 2, 2, 3, 3, 3, 4, 4, 5, 6]
     dado.sort(randOrd)
     return res.send({ jogadas : dado[0]}) 
 });
 
+/*
 router.post('/left', async (req,res)=> {
     const { nickname, socket, heroi ,id_sala } = req.body;
 
@@ -233,7 +262,7 @@ router.post('/left', async (req,res)=> {
     if(!resp.nModified) return res.status(400).send({ error: "Não foi possível sair da sala.", id_sala}) 
 
     return res.send({ status: "O player saiu da sala."}) 
-});
+});*/
 
 
 module.exports = app => app.use('/salas', router);
