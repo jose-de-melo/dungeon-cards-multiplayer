cf1794e2e16e725a1a67aa6d26b188c0a7bd8071
José de Melo <josedemelo.if@gmail.com>
2019-12-13 21:30:42 -0200

    [ADD] Renderizando matriz via socket.io

---
+++ app/assets/imagens/index.js
@@ -0,0 +1,46 @@
+const imagens =  {
+    monstro:{
+        alien: require('./monstro/alien.png'),
+        aranha: require('./monstro/aranha.png'),
+        cogumelo: require('./monstro/cogumelo.png'),
+        esqueleto: require('./monstro/esqueleto.png'),
+        javali: require('./monstro/javali.png'),
+        medusa: require('./monstro/medusa.png'),
+        zumbi: require('./monstro/zumbi.png'),
+        morcego: require('./monstro/morcego.png'),
+    },
+    heroi:{
+        androide: require('./heroi/androide.png'),
+        ceifadora: require('./heroi/ceifadora.png'),
+        barbaro: require('./heroi/barbaro.png'),
+        elfo: require('./heroi/elfo.png'),
+        necromante: require('./heroi/necromante.png'),
+        ninja: require('./heroi/ninja.png'),
+        templario: require('./heroi/templario.png')
+    },
+    heroi_armado:{
+        androide: require('./heroi_armado/androide.png'),
+        ceifadora: require('./heroi_armado/ceifadora.png'),
+        barbaro: require('./heroi_armado/barbaro.png'),
+        elfo: require('./heroi_armado/elfo.png'),
+        necromante: require('./heroi_armado/necromante.png'),
+        ninja: require('./heroi_armado/ninja.png'),
+        templario: require('./heroi_armado/templario.png')
+    },
+    arma:{
+        androide: require('./arma/androide.png'),
+        ceifadora: require('./arma/ceifadora.png'),
+        barbaro: require('./arma/barbaro.png'),
+        elfo: require('./arma/elfo.png'),
+        necromante: require('./arma/necromante.png'),
+        ninja: require('./arma/ninja.png'),
+        templario: require('./arma/templario.png')
+    },
+    item:{
+        moeda: require('./item/moeda.png'),
+        potion: require('./item/potion.png'),
+    }, 
+
+}
+
+export default imagens;
--- app/src/pages/room/Room.js
+++ app/src/pages/room/Room.js
@@ -3,6 +3,7 @@ import { Text, FlatList, View, Dimensions, StyleSheet, Image, TouchableOpacity, 
 
 import AsyncStorage from '@react-native-community/async-storage'
 import io from 'socket.io-client'
+import imagens  from './../../../assets/imagens'
 
 const config = require('../../config/config')
 
@@ -17,6 +18,8 @@ export default function Room({ navigation }){
     const [data, setData] = useState([])
     const [user, setUser] = useState('')
     const [emit, setEmit] = useState(true)
+    const [gameOn, setGameOn] = useState(false)
+    const [players, setPlayers] = useState(0)
 
     const getUser = async () => {
         const nick = await AsyncStorage.getItem('nickname')
@@ -37,44 +40,81 @@ export default function Room({ navigation }){
         setData(JSON.parse(matriz))
     })
 
+    socket.on('newPlayer', numberOfPlayers => {
+        console.log("NEW PLAYER")
+        setPlayers(numberOfPlayers)
+    })
+
+    socket.on('gameStart', numberOfPlayers => {
+        console.log(numberOfPlayers)
+        setGameOn(true)
+    })
+
     function move(item, index){
         socket.emit('move', index)
     }
 
+    function exitOfGame(){
+        socket.close()
+    }
+
     const renderItem = ({ item, index }) => {
-        console.log(item[0].name)
+        console.log(item)
         return (
-            <TouchableOpacity style={styles.item}>
+            <TouchableOpacity style={styles.item} onPress={exitOfGame}>
                  <View>
-                    <Image source={require('../../images/hero.png')} style={styles.image}/>
+                    <Image
+                        source={imagens[item.tipo][item.image]}
+                        style={styles.image}
+                    />
                 </View>
             </TouchableOpacity>
         )
     }
 
 
-    return (
-            <FlatList
-                keyExtractor={(_, index) => index}
-                contentContainerStyle={styles.container}
-                numColumns={numberGrid} data={data}
-                renderItem={renderItem}
-            />
-    )
+    return ( <>
+                {
+                    (gameOn) 
+                    ?
+                            <FlatList
+                                keyExtractor={(_, index) => index}
+                                contentContainerStyle={styles.container}
+                                numColumns={numberGrid} data={data}
+                                renderItem={renderItem}
+                            />
+                    : 
+                    <>
+                        <StatusBar translucent backgroundColor={styles.waiting.backgroundColor}/>
+                        <View style={styles.waiting}>
+                            <Text style={styles.textWaiting}>
+                                Aguardando{"\n\n"}players...
+                            </Text>
+
+                            <Text style={styles.textWaiting}>
+                                {"\n"}{players}/4
+                            </Text>
+                        </View>
+                    </>
+                }
+    
+    
+    </> )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginVertical: 130,
+        marginVertical: 130
     },
+
     item: {
         backgroundColor: '#4D243D',
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1,
         margin: 1,
-        height: Dimensions.get('window').width / numColumns, // approximate a square
+        height: Dimensions.get('window').width / numColumns
     },
     itemInvisible: {
         backgroundColor: 'transparent',
@@ -86,5 +126,20 @@ const styles = StyleSheet.create({
         flex: 1,
         width: 80,
         resizeMode: 'contain'
+    },
+
+    waiting: {
+        flex: 1,
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+        backgroundColor: "#263238"
+    },
+
+    textWaiting: {
+        fontFamily: "PressStart",
+        color: '#FFF',
+        fontSize: 25,
+        
     }
 });
