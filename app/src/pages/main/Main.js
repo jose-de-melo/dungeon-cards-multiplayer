baaafd8c42fb882b31d7aeb930b9e6c78a899e00
Lucas Heber <lucas.heber07@gmail.com>
2019-12-14 14:50:27 -0300

    [UP]

--- app/src/pages/main/Main.js
+++ app/src/pages/main/Main.js
@@ -4,7 +4,7 @@ import data from './data'
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.105:3000/',
+    baseURL: 'http://192.168.1.105:3000/',
 });
 
 let numberGrid = 6
@@ -16,76 +16,87 @@ export default class App extends Component {
         data: data
     }
 
-    componentDidMount(){
-        this.obterposicoes()
+    componentDidMount() {
+        // this.obterposicoes()
         this.iniciar()
     }
 
-    movimentar = (item, index) => {
-        let jogador = this.state.data[postAtual]
-        this.state.data[postAtual] = item
-        this.state.data[index] = jogador
-        postAtual = index
+    movimentar = async (item, index) => {
+        const response = await api.post('/game/movimentar/', { x_atual: item, y_atual: 4, x_mov: 2, y_mov: 4 })
 
-        this.setState(this.state)
-        console.log()
+        if (response.message == 0) {
+            alert("Movimento Inválido!")
+
+        } else {
+            let jogador = this.state.data[postAtual]
+            this.state.data[postAtual] = item
+            this.state.data[index] = jogador
+            postAtual = index
+
+            this.setState(this.state)
+            
+            console.log(response)
+
+        }
     };
 
-    iniciar = async () => {
-        // const response = await api.post('/game/join/', {nickname: "Lucas Heber", socket: 1})
-        // const response = await api.post('/game/join/', {nickname: "Domith", socket: 2})
-        // const response = await api.post('/game/join/', {nickname: "Ricardo", socket: 3})
-        const response = await api.post('/game/join/', {nickname: "Outro Ze", socket: 4})
 
-        console.log(response)
+
+        iniciar = async () => {
+            // const response = await api.post('/game/join/', {nickname: "Lucas Heber", socket: 1})
+            // const response = await api.post('/game/join/', {nickname: "Domith", socket: 2})
+            // const response = await api.post('/game/join/', {nickname: "Ricardo", socket: 3})
+            const response = await api.post('/game/join/', { nickname: "Outro Ze", socket: 4 })
+
+            console.log(response)
+        }
+
+        obterposicoes = async () => {
+            const response = await api.get('/game/iniciar')
+            console.log(response)
+        }
+
+        renderItem = ({ item, index }) => {
+            return (
+                <TouchableOpacity style={styles.item} onPress={() => this.movimentar(item, index)}>
+                    <View>
+                        <Image source={item.avatar} style={styles.image} />
+                    </View>
+                </TouchableOpacity>
+            )
+        }
+
+        render() {
+            return <FlatList
+                keyExtractor={(_, index) => index}
+                contentContainerStyle={styles.container}
+                numColumns={numberGrid} data={this.state.data}
+                renderItem={this.renderItem} />
+        }
     }
 
-    obterposicoes = async () => {
-        const response = await api.get('/game/iniciar')
-        console.log(response)
-    }
-
-    renderItem = ({ item, index }) => {
-        return (
-            <TouchableOpacity style={styles.item} onPress={() => this.movimentar(item, index)}>
-                 <View>
-                    <Image source={item.avatar} style={styles.image}/>
-                </View>
-            </TouchableOpacity>
-        )
-    }
-
-    render() {
-        return <FlatList
-            keyExtractor={(_, index) => index}
-            contentContainerStyle={styles.container}
-            numColumns={numberGrid} data={this.state.data}
-            renderItem={this.renderItem} />
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginVertical: 130,
-    },
-    item: {
-        backgroundColor: '#4D243D',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        margin: 1,
-        height: Dimensions.get('window').width / numColumns, // approximate a square
-    },
-    itemInvisible: {
-        backgroundColor: 'transparent',
-    },
-    itemText: {
-        color: '#fff',
-    },
-    image: {
-        flex: 1,
-        width: 80,
-        resizeMode: 'contain'
-    }
-});
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            marginVertical: 130,
+        },
+        item: {
+            backgroundColor: '#4D243D',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flex: 1,
+            margin: 1,
+            height: Dimensions.get('window').width / numColumns, // approximate a square
+        },
+        itemInvisible: {
+            backgroundColor: 'transparent',
+        },
+        itemText: {
+            color: '#fff',
+        },
+        image: {
+            flex: 1,
+            width: 80,
+            resizeMode: 'contain'
+        }
+    });
